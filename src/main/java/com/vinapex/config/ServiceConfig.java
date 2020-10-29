f099fd6b6bdc0aef92117aef0ad78e8c57a92900
Vinodh Thiagarajan <vinodh.thiagarajan@gmail.com>
2020-10-29 09:14:20 -0500

    Made this to consume and parse JWT

---
+++ src/main/java/com/vinapex/config/ServiceConfig.java
@@ -0,0 +1,17 @@
+package com.vinapex.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+@Configuration
+public class ServiceConfig {
+    //@Value("${signing.key}")
+    private String jwtSigningKey="345345fsdfsf5345";
+
+    public String getJwtSigningKey() {
+        return jwtSigningKey;
+    }
+
+}
--- src/main/java/com/vinapex/filters/TrackingFilter.java
+++ src/main/java/com/vinapex/filters/TrackingFilter.java
@@ -3,6 +3,9 @@ package com.vinapex.filters;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import com.vinapex.config.ServiceConfig;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +17,9 @@ public class TrackingFilter extends ZuulFilter {
     private static final int FILTER_ORDER = 1;
     private static final boolean SHOULD_FILTER = true;
     private static final Logger logger = LoggerFactory.getLogger(TrackingFilter.class);
+
+    @Autowired
+    ServiceConfig serviceConfig;
 
     @Autowired
     FilterUtils filterUtils;
@@ -34,7 +40,7 @@ public class TrackingFilter extends ZuulFilter {
     }
 
     private boolean isCorrelationIdPresent() {
-        if(filterUtils.getCorrelationId() != null) {
+        if (filterUtils.getCorrelationId() != null) {
             return true;
         }
         return false;
@@ -46,8 +52,8 @@ public class TrackingFilter extends ZuulFilter {
 
     @Override
     public Object run() throws ZuulException {
-        if(isCorrelationIdPresent()) {
-            logger.info("tmx-correlation-id found in tracking filter : {}.",filterUtils.getCorrelationId());
+        if (isCorrelationIdPresent()) {
+            logger.info("tmx-correlation-id found in tracking filter : {}.", filterUtils.getCorrelationId());
         } else {
             filterUtils.setCorrelationId(generateCorrelationId());
         }
@@ -55,7 +61,27 @@ public class TrackingFilter extends ZuulFilter {
         logger.info("tmx-correlation-id generated in tracking filter : {}.", filterUtils.getCorrelationId());
 
         RequestContext ctx = RequestContext.getCurrentContext();
-        logger.info("Processing incoming Request for {}.",ctx.getRequest().getRequestURI());
+        System.out.println("The organization id from the token is : " + getOrganizationId());
+        filterUtils.setOrgId(getOrganizationId());
+        logger.debug("Processing incoming request for {}.",  ctx.getRequest().getRequestURI());
         return null;
+    }
+
+    private String getOrganizationId() {
+        String result = "";
+        String authToken = "";
+        if (filterUtils.getAuthToken() != null) {
+            authToken = filterUtils.getAuthToken().replace("Bearer ", "");
+        }
+
+        try {
+            Claims claims = Jwts.parser().setSigningKey(serviceConfig.getJwtSigningKey()
+                    .getBytes("UTF-8")).parseClaimsJws(authToken).getBody();
+            result = (String) claims.get("organizationId");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
